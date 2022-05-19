2962903e1cb9fca7cd4042971acd28b662fa3476
alphaEmpyrean <dev@alphaempyrean.com>
2022-05-19 02:57:06 -0500

    Change from constructor to field autowired DI

--- springAutoFieldDI/src/main/java/com/etherealhazel/spring/core/demo/app/domain/Human.java
+++ springAutoFieldDI/src/main/java/com/etherealhazel/spring/core/demo/app/domain/Human.java
@@ -1,21 +1,16 @@
 package com.etherealhazel.spring.core.demo.app.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Human {
     
+    @Autowired
     public Pants pants;
     
     public Human() {
-    }
-
-    @Autowired
-    public Human(@Qualifier("cargoPants") Pants pants) {
-        this.pants = pants;
-    }
+    }    
 
     public void goOutside() {
         System.out.println("I want to go outside, better put on some pants!");
